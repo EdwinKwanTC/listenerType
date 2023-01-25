696dafaf496a22ce73abfd8719d30a3df1825683
Edwin Kwan <edwinkwantc@gmail.com>
2023-01-25 13:31:41 +0800

    fix: remove unnessary type

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { Request, Response } from "express";
-import * as http from "http";
 
 const app = express();
 
@@ -10,11 +9,7 @@ app.get("/", function (req: Request, res: Response) {
 
 const PORT = 8080;
 
-interface Listener extends http.Server {
-  address(): { port: number; family: string; address: string } | null | string;
-}
-
-const listener: Listener = app.listen(PORT, () => {
+const listener = app.listen(PORT, () => {
   const address = listener.address();
   if (address !== null && typeof address === "object") {
     console.log(address.port);
